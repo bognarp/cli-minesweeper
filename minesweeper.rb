8f3eee2334a3b1ea24aa7589369ea3a5b9abcd9e
bognarp <21261487+bognarp@users.noreply.github.com>
2019-12-12 13:41:45 +0100

    Fix 'overlap' on board movements

--- minesweeper.rb
+++ minesweeper.rb
@@ -36,23 +36,13 @@ class Game
 
     def prompt_input
         puts "==============="
-        puts "Make your move! (Use the arrow keys and press 'R' to reveal or 'F' to flag a position)"
+        puts "Use the arrow keys and press 'R' to reveal or 'F' to flag a position"
         puts "---------------"
         puts "Press 'S' to save game"
         puts "Press 'L' to load game"
         puts "Press 'Q' to quit game"
         puts "==============="
     end
-
-    # def valid_number?(elem)
-    #     return false if elem.empty?
-    #     nums = "0123456789"
-    #     elem.chars.all? { |c| nums.include?(c) }
-    # end
-
-    # def valid_coordinate?(elem)
-    #     elem.to_i.between?(0,@board.grid_size-1)
-    # end
     
     def win_message
         system("clear")
@@ -70,6 +60,23 @@ class Game
         puts "=========="
     end
 
+    def one_step(num,direction)
+        case direction
+        when '-'
+            if num == 0
+                @board.grid_size-1
+            else
+                num-1
+            end
+        when '+'
+            if num == @board.grid_size-1
+                0
+            else
+                num+1
+            end
+        end
+    end
+
     def filter_moves(move)
         x,y = @selected_pos
         case move.to_s
@@ -78,24 +85,25 @@ class Game
         when "l"
             load_game
         when "up"
-            @selected_pos = [x-1,y]
+            @selected_pos = [one_step(x,'-'),y]
         when "down"
-            @selected_pos = [x+1,y]
+            @selected_pos = [one_step(x,'+'),y]
         when "right"
-            @selected_pos = [x,y+1]
+            @selected_pos = [x,one_step(y,'+')]
         when "left"
-            @selected_pos = [x,y-1]
+            @selected_pos = [x,one_step(y,'-')]
         when 'r'
             @board[x,y].reveal
         when 'f'
             @board[x,y].flag
+        when 'q'
+            @board.quit!
         end
     end
 
     def make_move
         user_input = Interaction.new
         user_input.loop do |key|
-            @board.quit! if key == ?q
             filter_moves(key)
             @board.select_tile(@selected_pos)
             break
